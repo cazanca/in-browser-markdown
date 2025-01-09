832390d907eb2e13bb4f7759ff24bc50a05caa6b
João Cazança <50326472+cazanca@users.noreply.github.com>
2025-01-09 14:40:16 +0200

    update readme

--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -1,7 +1,7 @@
 <template>
   <teleport to="body">
     <div v-if="isOpen" class="fixed inset-0 bg-custom-1000 bg-opacity-75 h-screen flex items-center justify-center transition-all duration-300">
-      <div class="space-y-4 max-w-[343px] max-h-[218px] p-6 rounded-md bg-custom-100">
+      <div class="space-y-4 max-w-[343px] max-h-[218px] p-6 rounded-md bg-custom-100 update-to-git">
         <h4 class="font-bold text-xl font-serif">Delete this document?</h4>
         <p class="font-serif text-sm text-custom-500">Are you sure you want to delete the ‘welcome.md’ document and its contents? This action cannot be reversed.</p>
         <JButton class="w-full" @click.prevent="emit('delete-close')">Confirm & Delete</JButton>
